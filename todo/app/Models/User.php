91d30b5564e6b20fbbae8a8b8642119520407060
cossack910 <maris160zhk@gmail.com>
2023-04-09 21:49:27 +0900

    #2

--- todo/app/Models/User.php
+++ todo/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -43,4 +44,13 @@ class User extends Authenticatable
     ];
 
     protected $table = 'users';
+
+    public static function userCreate($request)
+    {
+        User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+    }
 }
