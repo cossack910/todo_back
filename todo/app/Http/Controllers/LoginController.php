36cebe7bfce4a51420b36120efc5cb6e024d63b2
cossack910 <maris160zhk@gmail.com>
2023-04-10 23:09:15 +0900

    try catche追加

--- todo/app/Http/Controllers/LoginController.php
+++ todo/app/Http/Controllers/LoginController.php
@@ -2,16 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Log;
 use App\Http\Request\LoginRequest;
 use App\Models\User;
 use App\Common\LoginUtil;
 use App\Const\AuthConst;
+use Exception;
 
 class LoginController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        $token = User::userAuth($request);
+        try {
+            $token = User::userAuth($request);
+            if (null === $token) {
+                throw new Exception("ログイン認証失敗");
+            }
+            Log::info("ログイン認証成功");
+        } catch (Exception $e) {
+            Log::info($e);
+            return response()->json(
+                LoginUtil::retJsonArr(
+                    AuthConst::RESULTCODE_FAILED,
+                    AuthConst::MESSAGE_FAILED,
+                    ""
+                ),
+                400
+            );
+        }
+
         return response()->json(
             LoginUtil::retJsonArr(
                 AuthConst::RESULTCODE_SUCCESS,
